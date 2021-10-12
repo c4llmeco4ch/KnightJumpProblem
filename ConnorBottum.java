a0231ab89f0cdbdc81e29c2341e5ae478344aa8c
c4llmeco4ch <cbb692@gmail.com>
2021-10-12 12:03:49 -0400

    Working solution complete

--- ConnorBottum.java
+++ ConnorBottum.java
@@ -10,10 +10,12 @@ public class ConnorBottum{
 
     final static int MAX_CONSTRAINED = 2;
     final static char[] CONSTRAINED_CHARS = {'a', 'e', 'i', 'o', 'u', 'y'}; // sorted
+    final static char[] EMPTY_CHARS = {'-', ' '};
     static HashMap<Character, Integer[]> positions;
 
     public static void main(String[] args){
-        SolveMatrix();
+        long pathCount = SolveMatrix();
+        System.out.printf("There are %d total paths", pathCount);
     }
 
     /**
@@ -21,7 +23,7 @@ public class ConnorBottum{
      * Determines the total number of unique 8-cell paths
      * for the Knight from all starting locations.
      * 
-     * @return
+     * @return the total number of viable paths a Knight can take
      */
     public static long SolveMatrix(){
         char[][] grid = parseFile("test.txt");
@@ -33,12 +35,13 @@ public class ConnorBottum{
         ArrayList<String> pathQueue = initQueue(grid);
         
         /*
-        * 
+        * A Breadth-First implementation
+        * of what effectively boils down to a graph traversal
         */
         while(!pathQueue.isEmpty()){
             String current = pathQueue.remove(0);
             int l = current.length();
-            Character[] moves = calcJumps(current.charAt(l - 1), grid);
+            ArrayList<Character> moves = calcJumps(current.charAt(l - 1), grid);
             for(char c : moves){
                 String possibleMove = current + c;
                 if(isValid(possibleMove)){
@@ -55,15 +58,18 @@ public class ConnorBottum{
     }
 
     /**
+     * Initializes the queue for our breadth-first traversal
+     * by creating an ArrayList of the individual non-empty squares
      * 
-     * @param board
-     * @return
+     * @param board The 2D grid representing the board state
+     * @return An initial ArrayList containing length-1 strings
+     * of each individual non-empty square
      */
     private static ArrayList<String> initQueue(char[][] board){
         ArrayList<String> startQueue = new ArrayList<String>();
         for(int row = 0; row < board.length; row++){
             for(int col = 0; col < board[0].length; col++){
-                if(board[row][col] == '-' || board[row][col] == ' '){
+                if(isEmpty(board[row][col])){
                     continue;
                 }
                 startQueue.add(String.valueOf(board[row][col]));
@@ -73,62 +79,68 @@ public class ConnorBottum{
     }
 
     /**
+     * For the Knight on a given position, evaluate the 8 surrounding
+     * positions and determine which are valid jumps, elimiating any
+     * out-of-bounds jumps or jumps to empty squares
      * 
-     * @param curr
-     * @param board
-     * @return
+     * @param curr the character on which the Knight sits
+     * @param board the 2D grid representing the board state
+     * @return the list of valid jumps the Knight can make on its next move
      */
-    private static Character[] calcJumps(char curr, char[][] board){
+    private static ArrayList<Character> calcJumps(char curr, char[][] board){
         Integer pos[] = positions.get(curr);
         int y = pos[0];
         int x = pos[1];
+        System.out.printf("Its coordinates are (%d, %d)\n", y, x);
         ArrayList<Character> moves = new ArrayList<Character>();
         int left = (x < 1 ? 0 : x < 2 ? 1 : 2);
-        int right = (x >= board[0].length ? 0 : x > board[0].length - 1 ? 1 : 2);
+        int right = (x >= board[0].length - 1 ? 0 : x >= board[0].length - 2 ? 1 : 2);
         int top = (y < 1 ? 0 : y < 2 ? 1 : 2);
-        int bottom = (y >= board.length ? 0 : y > board.length - 1 ? 1 : 2);
+        int bottom = (y >= board.length - 1 ? 0 : y >= board.length - 2 ? 1 : 2);
         if(left >= 1){
-            if(top >= 2){
+            if(top >= 2 && !isEmpty(board[y - 2][x - 1])){
                 moves.add(board[y - 2][x - 1]);
             }
-            if(bottom >= 2){
+            if(bottom >= 2 && !isEmpty(board[y + 2][x - 1])){
                 moves.add(board[y + 2][x - 1]);
             }
         }
         if(left >= 2){
-            if(top >= 1){
+            if(top >= 1 && !isEmpty(board[y - 1][x - 2])){
                 moves.add(board[y - 1][x - 2]);
             }
-            if(bottom >= 1){
+            if(bottom >= 1 && !isEmpty(board[y + 1][x - 2])){
                 moves.add(board[y + 1][x - 2]);
             }
         }
         if(right >= 1){
-            if(top >= 2){
+            if(top >= 2 && !isEmpty(board[y - 2][x + 1])){
                 moves.add(board[y - 2][x + 1]);
             }
-            if(bottom >= 2){
+            if(bottom >= 2 && !isEmpty(board[y + 2][x + 1])){
                 moves.add(board[y + 2][x + 1]);
             }
         }
         if(right >= 2){
-            if(top >= 1){
+            if(top >= 1 && !isEmpty(board[y - 1][x + 2])){
                 moves.add(board[y - 1][x + 2]);
             }
-            if(bottom >= 1){
+            if(bottom >= 1 && !isEmpty(board[y + 1][x + 2])){
                 moves.add(board[y + 1][x + 2]);
             }
         }
-        return (Character[])moves.toArray();
+        return moves;
     }
 
-
     /**
+     * Evaluates if a path is valid, comparing the number of limited
+     * characters to the value denoted by <i>MAX_CONSTRAINED</i>
      * 
-     * @param s
-     * @return
+     * @param s the string representing a path
+     * @return whether the path passes a validation check or not
      */
     private static boolean isValid(String s){
+        if(s.length() <= MAX_CONSTRAINED){return true;}
         int totalConstrained = 0;
         for(int pos = 0; pos < s.length(); pos++){
             char c = s.charAt(pos);
@@ -140,6 +152,20 @@ public class ConnorBottum{
             }
         }
         return true;
+    }
+
+    /**
+     * Determines if a given character qualifies as a representative
+     * of an empty square
+     * 
+     * @param c the character to be evaluated
+     * @return whether the square <i>c</i> rests on is empty or not
+     */
+    private static boolean isEmpty(char c){
+        for(char em : EMPTY_CHARS){
+            if(c == em){return true;}
+        }
+        return false;
     }
 
     /**
