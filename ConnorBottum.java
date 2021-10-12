9f0353537e8f99e7e22f673d17a5e446c6778411
c4llmeco4ch <cbb692@gmail.com>
2021-10-11 23:29:19 -0400

    Initial java file structuring

--- ConnorBottum.java
+++ ConnorBottum.java
@@ -1,7 +1,41 @@
 import java.util.HashSet;
+import java.util.Scanner;
+import java.io.File;
 
 public class ConnorBottum{
+
     final int MAX_CONSTRAINED = 2;
     final char[] CONSTRAINED_CHARS = {'y', 'e', 'i', 'o', 'u', 'y'};
-    
+
+    public static void main(String[] args){
+
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public static long SolveMatrix(){
+        char[][] grid = parseFile("test.txt");
+        HashSet<String> paths = new HashSet<String>();
+        
+    }
+
+    /**
+     * Returns a 2-dimensional grid representing
+     * the board state a Knight will jumping on inside
+     * the SolveMatrix() method
+     * <p>
+     * The file will contain rows of chars representing the
+     * characters on the board. Empty spaces will either be
+     * represented with a ' ' (space) or a '-' (hyphen). An 
+     * example file might look like
+     * 
+     * @param path the file path for the input file
+     * @return     the 2D "chess board"
+     */
+    private static char[][] parseFile(String path){
+        Scanner inputFile = new Scanner(new File(path));
+
+    }
 }
